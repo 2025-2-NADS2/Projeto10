b5400afaccfbd56bdb126de0845f6dacb62f9ce0
Bianca Morais <bianca.morais@fecap.br>
2025-11-10 21:37:21 -0300

    Update inscricaoRoutes.js

--- src/Entrega 2/Desenvolvimento Web Full Stack/backend/routes/inscricaoRoutes.js
+++ src/Entrega 2/Desenvolvimento Web Full Stack/backend/routes/inscricaoRoutes.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const inscricaoController = require('../controllers/inscricaoController');
 const { checkAdmin } = require('../middleware/authMiddleware.js');
 
-// --- ROTA PÚBLICA (Para o Modal "Me Notifique") ---
-// (POST /api/inscricoes)
+
 router.post('/', inscricaoController.createInscricao);
 
-// --- ROTA PRIVADA (Para o Admin ver a lista) ---
-// (GET /api/inscricoes)
 router.get('/', checkAdmin, inscricaoController.getInscricoes);
 
 module.exports = router;
